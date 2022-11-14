634c82a7a8a72288796c3485dab2fad5e9571a44
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2022-11-14 16:35:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const errorMsg = document.querySelector(".error-msg");
+const loader = document.querySelector(".loader");
+const resultsDisplay = document.querySelector(".results-display");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === "") {
+    errorMsg.textContent = "Wops, veuillez remplir l'input";
+    return;
+  } else {
+    errorMsg.textContent = "";
+    loader.style.display = "flex";
+    resultsDisplay.textContent = "";
+    wikiApiCall(input.value);
+  }
+}
+
+async function wikiApiCall(searchInput) {
+  try {
+    const response = await fetch(
+      `https://fr.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+    );
+    // console.log(response);
+
+    // si erreur 404 et qu'ont veut la voir :
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    // on récupérer les données qui sont traduites au format json pour pouvoir ensuite les récupérer et affichervvvsv
+    const data = await response.json();
+    // console.log(data);
+
+    // fonction qui va créer des cartes pour présenter les données récupérées au format html
+    createCards(data.query.search);
+  } catch (error) {
+    errorMsg.textContent = `${error}`;
+    loader.style.display = "none";
+  }
+}
+
+function createCards(data) {
+  if (!data.length) {
+    errorMsg.textContent = "Wopsy, aucun résultat";
+    loader.style.display = "none";
+    return;
+  }
+  data.forEach((el) => {
+    const url = `https://fr.wikipedia.org/?curid=${el.pageid}`;
+    const card = document.createElement("div");
+    card.className = "results-item";
+    card.innerHTML = `
+      <h3 class="result-title">
+        <a href=${url} target="_blank">${el.title}</a>
+      </h3>
+      <a href=${url} class="result-link" target="_blank">${url}</a>
+      <span class="result-snippet">${el.snippet}</span>
+      <br>
+    `;
+    // rajoute un enfnat à resultsDisplay
+    resultsDisplay.appendChild(card);
+  });
+  loader.style.display = "none";
+}
